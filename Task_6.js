405b88d50693b93d5c5a458604cb037fb58d5806
IDuckOdd <79915759+IDuckOdd@users.noreply.github.com>
2024-10-13 23:34:39 +0700

    Add files via upload

---
+++ Task_6.js
@@ -0,0 +1,14 @@
+function left_column() {
+    document.getElementById('pleft').className = 'pleft-left';
+    document.getElementById('pright').className = 'pright-left';
+}
+
+function both_column() {
+    document.getElementById('pleft').className = 'pleft-both';
+    document.getElementById('pright').className = 'pright-both';
+}
+
+function right_column() {
+    document.getElementById('pleft').className = 'pleft-right';
+    document.getElementById('pright').className = 'pright-right';
+}
